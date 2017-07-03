d6123fd67f7c5177b7d6602d543145598e0e2d05
JP Ubana <jpjubana@gmail.com>
2017-07-03 19:37:04 -0400

    resubmission

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 /*****************************Modules***************************/
 
-const express = require('express')
-const parseurl = require('parseurl')
-const session = require('express-session')
+const express = require('express');
+const session = require('express-session');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const jsonfile = require('jsonfile');
 const fs = require('fs');
 
-const app = express()
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-const randomIndex = Math.floor(Math.random() * words.length);
-const randomWord = words[randomIndex];
-
-let wordLetters = { "wletters": randomWord.split('') };
+const app = express();
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
-app.set('view engine', 'mustache')
+app.set('view engine', 'mustache');
 
 /*****************************Middleware***************************/
 
@@ -27,45 +19,85 @@ app.use('/css', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
-    secret: 'keyboard cat',
+    secret: 'Wubba Lubba Dub Dub',
     resave: false,
     saveUninitialized: true
-}))
-
-// app.use(function(req, res, next) {
-//     const guessWord = req.session.randomWord;
-//     if (!guessWord) {
-//         res.redirect('/')
-//     }
-
-//     next()
-// });
+}));
 
 /**********************************Routes***************************/
 
+let words = fs.readFileSync("/usr/share/dict/words", "utf-8").toUpperCase().split("\n");
+let randomIndex = Math.floor(Math.random() * words.length);
+let randomWord = words[randomIndex];
+let wordLetters = randomWord.split('');
+let gameWord = wordLetters;
+let gameLife = 8;
+let guessWord = [];
+
+console.log(gameWord);
+
+function blanks(arr) {
+    for (let i in arr) {
+        guessWord.push(' ');
+    }
+};
+blanks(wordLetters);
+
+let lettersBucket = [];
+
+let gameWin = true;
+let gameLose = true;
+
 app.get('/', function(req, res) {
-    res.render('index', wordLetters);
-})
+    req.session.gameWin = gameWin;
+    req.session.gameLose = gameLose;
+    req.session.guessWord = guessWord;
+    req.session.lettersBucket = lettersBucket;
+    req.session.gameLife = gameLife;
+    req.session.gameWord = gameWord;
 
-console.log(wordLetters);
+    if (!gameWord) {
+        let randomWord = words[randomIndex];
+        let wordLetters = randomWord.split('');
+        gameWord = req.session.gameWord = wordLetters;
+    }
 
-// app.get("/", function(req, res) {
-//     if (Object.keys(req.session.credntls) === 'username') {
-//         res.send("Welcome, " + Object.keys(req.session.credntls));
-//     } else {
-//         res.render('index');
-//     }
-// });
+    let tags = { gameWord: gameWord, guessWord: guessWord, gameLife: gameLife, lettersBucket: lettersBucket, gameWin: gameWin, gameLose: gameLose };
+    res.render('index', { tags: tags });
+});
 
-// app.post("/login", function(req, res) {
-//     const users = {};
-//     Object.defineProperty(users, req.body.username, { value: req.body.password });
-//     if (req.session.credntls.username === users.username) {
-//         res.redirect('/');
-//     } else {
-//         res.send("Try again sucka!");
-//     }
-// });
+app.post("/", function(req, res) {
+    req.session.gameLife = gameLife;
+    let letterGuess = req.body.letter.toUpperCase();
+    req.session.gameWord = wordLetters;
+    req.session.guessWord = guessWord;
+    req.session.lettersBucket = lettersBucket;
+    if (lettersBucket.includes(letterGuess)) {
+        res.send("pick another letter!");
+    } else if (gameWord.includes(letterGuess)) {
+        for (let i in gameWord) {
+            if (letterGuess === gameWord[i]) {
+                guessWord.splice(i, 1, letterGuess);
+            }
+        }
+        lettersBucket.push(letterGuess)
+    } else if (!gameWord.includes(letterGuess)) {
+        lettersBucket.push(letterGuess);
+        gameLife--
+    }
+
+    if (!guessWord.includes(' ')) {
+        gameWin = false;
+        res.redirect('/');
+    }
+
+    if (gameLife === 0) {
+        gameLose = false;
+        res.redirect('/');
+    }
+
+    res.redirect('/');
+});
 
 /**********************************Get shit going***************************/
 
